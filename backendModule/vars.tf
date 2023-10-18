296726a9213c103a64ebe3d9c71d01134b613fff
Snaatak-priya <142471903+Snaatak-priya@users.noreply.github.com>
2023-10-18 17:04:45 +0530

    Update vars.tf
    adding values to default block in vars.tf

--- backendModule/vars.tf
+++ backendModule/vars.tf
@@ -1,65 +1,65 @@
 variable "dynamodb_table_name" {
     type = string
-    default = ""
+    default = "snaatak-project-lock-table"
 }
 
 variable "billing_mode" {
     type = string
-    default = ""
+    default = "PROVISIONED"
 
 }
 
 variable "read_capacity" {
     type = string
-    default = ""
+    default = "5"
 }
 
 variable "write_capacity" {
     type = string
-    default = ""
+    default = "5"
 
 }
 
 variable "hash_key" {
     type = string
-    default = ""
+    default = "LockID"
 
 }
 
 variable "s3_bucket_name" {
     type = string
-    default = ""
+    default = "snaatak-project-tfstate-storage"
 
 }
 
 
 variable "s3_versioning_status" {
     type = string
-    default = ""
+    default = "Enabled"
 }
 
 variable "s3_sse_algorithm" {
     type = string
-    default = ""
+    default = "AES256"
 
 }
 
 variable "s3_block_public_acls" {
     type = bool
-    default = ""
+    default = "true"
 }
 
 variable "s3_block_public_policy" {
     type = bool
-    default = ""
+    default = "true"
 }
 
 variable "s3_ignore_public_acls" {
     type = bool
-    default = ""
+    default = "true"
 }
 
 variable "s3_restrict_public_buckets" {
     type = bool
-    default = ""
+    default = "true"
 }
