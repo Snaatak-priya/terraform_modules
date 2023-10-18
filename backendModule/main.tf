73767ca8347212b27980e94b585d590b6f63c483
Snaatak-priya <142471903+Snaatak-priya@users.noreply.github.com>
2023-10-18 12:42:14 +0530

    Create main.tf
    Adding S3 bucket & dynamodb table

---
+++ backendModule/main.tf
@@ -0,0 +1,52 @@
+resource "aws_dynamodb_table" "dynamodb_table"{
+  name = var.dynamodb_table_name
+  billing_mode = var.billing_mode
+  read_capacity = var.read_capacity
+  write_capacity = var.write_capacity
+  hash_key = var.hash_key
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = var.dynamodb_table_name
+    Environment = var.env_name
+  }
+
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.s3_bucket_name
+ 
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = false #var.s3_prevent_destroy
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = var.s3_versioning_status
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = var.s3_sse_algorithm
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = var.s3_block_public_acls
+  block_public_policy     = var.s3_block_public_policy
+  ignore_public_acls      = var.s3_ignore_public_acls
+  restrict_public_buckets = var.s3_restrict_public_buckets
+}
