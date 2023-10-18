8c9c433486b620da4b70727f724529066305bd06
Snaatak-priya <142471903+Snaatak-priya@users.noreply.github.com>
2023-10-18 17:01:02 +0530

    Update vars.tf
    Adding     default = ""

--- backendModule/vars.tf
+++ backendModule/vars.tf
@@ -1,48 +1,65 @@
 variable "dynamodb_table_name" {
     type = string
+    default = ""
 }
 
 variable "billing_mode" {
     type = string
+    default = ""
+
 }
 
 variable "read_capacity" {
     type = string
+    default = ""
 }
 
 variable "write_capacity" {
     type = string
+    default = ""
+
 }
 
 variable "hash_key" {
     type = string
+    default = ""
+
 }
 
 variable "s3_bucket_name" {
     type = string
+    default = ""
+
 }
 
 
 variable "s3_versioning_status" {
     type = string
+    default = ""
 }
 
 variable "s3_sse_algorithm" {
     type = string
+    default = ""
+
 }
 
 variable "s3_block_public_acls" {
     type = bool
+    default = ""
 }
 
 variable "s3_block_public_policy" {
     type = bool
+    default = ""
 }
 
 variable "s3_ignore_public_acls" {
     type = bool
+    default = ""
 }
 
 variable "s3_restrict_public_buckets" {
     type = bool
+    default = ""
 }
